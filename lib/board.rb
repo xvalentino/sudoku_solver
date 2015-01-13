8edc5b791ed6aab6c7d8a0bf0b6e34ae4334a3cc
Valentino Espinoza <xvalentino13@gmail.com>
2015-01-13 13:03:57 -0700

    row method instad of name variables

--- lib/board.rb
+++ lib/board.rb
@@ -7,6 +7,8 @@ class Board
 
   def initialize
     blank_board
+    @row1 = row(1)
+    @row2 = row(2)
   end
 
   def blank_board
@@ -15,32 +17,16 @@ class Board
 
   def make_9_9_board
     @state = Array.new(9) {Array.new(9) {Spot.new}}
-    name_rows
   end
 
-  def intake_row_1(row)
-    row = row.to_s.chars
-    row.each_index do |index|
-    @row1[index].data = row[index]
+  def intake_row(row_number, numbers)
+    numbers = numbers.to_s.chars
+    numbers.each_index do |index|
+      row(row_number)[index].data = numbers[index]
     end
   end
 
-  def intake_row_2(row)
-    row = row.to_s.chars
-    row.each_index do |index|
-      @row2[index].data = row[index]
-    end
-  end
-
-  def name_rows
-    @row1 = @state[0]
-    @row2 = @state[1]
-    @row3 = @state[2]
-    @row4 = @state[3]
-    @row5 = @state[4]
-    @row6 = @state[5]
-    @row7 = @state[6]
-    @row8 = @state[7]
-    @row9 = @state[8]
+  def row(num)
+    @state[num - 1]
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -13,13 +13,13 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_takes_numbers
-    @board.intake_row_1(826594317)
+    @board.intake_row(1, 826594317)
     assert @board.row1
   end
 
   def test_board_takes_two_rows
-    @board.intake_row_1(826594317)
-    @board.intake_row_2(715638942)
+    @board.intake_row(1, 826594317)
+    @board.intake_row(2, 715638942)
     assert @board.row1
     assert @board.row2
   end
