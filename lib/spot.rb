717b92a40ffb84e805e9b418b21911a04b05307a
Valentino Espinoza <xvalentino13@gmail.com>
2015-01-12 23:15:28 -0700

    spot can only be single digit number.

--- lib/spot.rb
+++ lib/spot.rb
@@ -3,4 +3,16 @@ class Spot
   def initialize
     @data = ''
   end
+
+  def set_data(value)
+    if is_valid_one_digit_number?(value)
+      @data = value
+    else
+      @data = ''
+    end
+  end
+
+  def is_valid_one_digit_number?(value)
+    (1..9).to_a.include?(value.to_i) && value.length == 1
+  end
 end
--- test/spot_test.rb
+++ test/spot_test.rb
@@ -12,4 +12,11 @@ class SpotTest < Minitest::Test
     spot = Spot.new
     assert_equal '', spot.data
   end
+
+  def test_can_only_be_1_through_9
+    spot = Spot.new
+    other_spot = Spot.new
+    assert_equal '1', spot.set_data("1")
+    assert_equal '', other_spot.set_data("a")
+  end
 end
